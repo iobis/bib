b9759eb849aad0325b635a367bda33c06222c317
Pieter Provoost <pieterprovoost@gmail.com>
2019-09-18 20:00:24 +0200

    refactoring

---
+++ obis/affiliations.py
@@ -0,0 +1,170 @@
+# sudo pip install --ignore-installed six
+
+import requests
+import re
+from html.parser import HTMLParser
+import html
+import mechanize
+import time
+import sqlite3
+import lib
+import logging
+from termcolor import colored
+
+logging.basicConfig(level=logging.ERROR, format="%(asctime)s %(levelname)-8s %(message)s", datefmt="%H:%M:%S")
+logger = logging.getLogger("bib")
+logger.setLevel(logging.DEBUG)
+
+# config
+
+search_url = "https://apps.webofknowledge.com"
+base_url = "https://apps.webofknowledge.com/"
+max_results = 3
+log_to_html = True
+reprint = True
+offset = None
+
+# fetch OBIS publications (brefid and title) from API and order by brefid
+
+publications = requests.get("https://api.obis.org/publications/2019").json()["results"]
+publications = sorted(publications, key=lambda p: p["brefid"])
+
+# set up sqlite
+
+conn = sqlite3.connect("affiliations.db")
+lib.create_tables(conn)
+c = conn.cursor()
+
+# process
+
+h = HTMLParser()
+br = mechanize.Browser()
+br.set_handle_robots(False)
+
+for pub in publications:
+
+    title = pub["standardtitle"]
+    brefid = pub["brefid"]
+
+    logger.info(colored(pub["standardtitle"], "white"))
+
+    if offset is not None and id < offset:
+        continue
+
+    # clear publication and authors in sqlite
+
+    lib.database_cleanup(conn, brefid)
+
+    logger.debug("Opening search page")
+    br.open(search_url)
+    logger.debug("Received page, performing search")
+    br.select_form(name="WOS_GeneralSearch_input_form")
+    br["value(input1)"] = "\"" + lib.clean_title(title) + "\""
+    res = br.submit()
+    content = res.read().decode("utf-8")
+    logger.debug("Received search results")
+
+    if log_to_html:
+        with open("log/" + str(brefid) + ".html", "w") as f:
+            f.write(content)
+            logger.debug("Response written to file " + str(brefid) + ".html")
+
+    warnings = re.findall("STARTING A NEW SESSION", content)
+    if len(warnings) > 0:
+        logger.error("New session needed")
+        br._ua_handlers["_cookies"].cookiejar.clear()
+        br.open("http://www.webofknowledge.com", timeout=120)
+        time.sleep(10)
+        continue
+
+    warnings = re.findall("To run more searches", content)
+    if len(warnings) > 0:
+        logger.error("Search history full")
+        br._ua_handlers["_cookies"].cookiejar.clear()
+        br.open("http://www.webofknowledge.com", timeout=120)
+        logger.info("Sleeping for 10 seconds")
+        time.sleep(10)
+        continue
+
+    urls = re.findall("full_record\.do\?[^\"]*", content)
+    if len(urls) > 0:
+        logger.debug(colored("Found " + str(len(urls)) + " full record URLs", "cyan"))
+    else:
+        logger.debug("Found 0 full record URLs")
+
+    if 0 < len(urls) <= max_results:
+        full = html.unescape(base_url + urls[0])
+        logger.debug("Getting full record page: " + full)
+        r = requests.get(full, timeout=240)
+        page = r.text
+        logger.debug("Received page")
+
+        if log_to_html:
+            with open("log/" + str(brefid) + "_page.html", "w") as f:
+                f.write(content)
+                logger.debug("Response written to file " + str(brefid) + "_page.html")
+
+        # authors
+
+        authors = re.findall("author_name=(.*?)&amp", page)
+        logger.info("Found " + str(len(authors)) + " authors")
+
+        for author in authors:
+            query = "insert into authors values (%s, '%s')" % (id, author.replace("'", ""))
+            c.execute(query)
+            conn.commit()
+
+        # author addresses
+
+        found = False
+
+        addresses = re.findall("addressWOS(.*),(.*?)</a>", page)
+        for a in addresses:
+            address = a[1]
+
+            for country in lib.countries:
+                n = address.lower().count(country.lower())
+                if n > 0:
+
+                    print("\t" + country)
+                    found = True
+
+                    # insert record
+
+                    query = "insert into wos values (%s, '%s')" % (id, country)
+                    c.execute(query)
+                    conn.commit()
+
+                    break
+
+            if not found:
+                logger.warning("\tNo country found in \"" + address + "\"")
+
+        # reprint address
+
+        addresses = re.findall("fr_address_row2\">(.*?)<", page)
+
+        for address in addresses:
+
+            for country in lib.countries:
+                n = address.lower().count(country.lower())
+                if n > 0:
+
+                    logger.info("\tReprint address: " + country)
+                    found = True
+
+                    # insert record
+
+                    if reprint and not found:
+                        query = "insert into imis.wos values (%s, '%s')" % (id, country)
+                        c.execute(query)
+                        conn.commit()
+                        logger.info("\tUsed reprint address")
+
+                    break
+
+            if not found:
+                logger.warning("\tNo country found in \"" + address + "\"")
+
+c.close()
+conn.close()
--- obis/belgium/bib.py
+++ obis/belgium/bib.py
@@ -3,11 +3,8 @@ from suds.client import Client
 import requests
 import time
 import csv
-import sys
 
-filename = "lists/WoRMS_english_vernaculars_marine_20190328.csv"
-userQuery = "TS=%s AND AD=Belgi* AND PY=(2016-2019)"
-populate = True
+userQuery = "TS=(OBIS) AND AD=Belgi* AND PY=(%s)"
 
 # wsdl
 
@@ -18,27 +15,8 @@ wsdl_search = "http://search.webofknowledge.com/esti/wokmws/ws/WokSearchLite?wsd
 
 db = sqlite3.connect("bib.db")
 db.execute("create table if not exists auth (key text)")
-db.execute("create table if not exists species (name text, checked boolean)")
-db.execute("create table if not exists publications (uid text, species text, title text)")
+db.execute("create table if not exists publications (uid text, title text, year integer)")
 db.commit()
-
-# create species list
-
-if populate:
-	with open(filename, "rb") as csvfile:
-		reader = csv.reader(csvfile)
-		for row in reader:
-			sp = row[0].decode("utf8")
-			print sp
-			res = db.execute("select count(*) from species where name = ?", (sp,)).fetchone()[0]
-			if res == 0:
-				db.execute("insert into species values (?, ?)", (sp, False))
-				db.commit()
-
-# read species list
-
-specieslist = db.execute("select name from species where not checked").fetchall()
-specieslist = [element for tupl in specieslist for element in tupl]
 
 # authenticate
 
@@ -54,18 +32,11 @@ def refreshkey():
 
 key = refreshkey()
 
-#
-#keys = db.execute("select key from auth").fetchone()
-#if (keys is not None and len(keys) > 0):
-#	key = keys[0]
-#	print "Authentication key from database: %s" % key
-#else:
-
 # search
 
 client = Client(wsdl_search)
 
-for species in specieslist:
+for year in range(2000, 2020):
 
 	session_requests = session_requests + 1
 
@@ -81,11 +52,10 @@ for species in specieslist:
 	retrieve.firstRecord = 1
 	retrieve.count = 100
 
-	print "%s - Looking for publications on %s" % (session_requests, species)
+	print "%s - Looking for publications for %s" % (session_requests, year)
 
-	search = species.replace("(", "")
-	search = search.replace(")", "")
-	query.userQuery = userQuery % search
+	query.userQuery = userQuery % year
+	print query.userQuery
 
 	try:
 		results = client.service.search(query, retrieve)
@@ -95,15 +65,12 @@ for species in specieslist:
 
 				uid = result.uid
 				title = result.title[0][1][0]
-				print "Publication %s for %s: %s" % (uid, species, title)
+				print "Publication %s: %s" % (uid, title)
 
 				db.execute("delete from publications where uid = ?", (uid,))
-				db.execute("insert into publications values (?, ?, ?)", (uid, species, title))
+				db.execute("insert into publications values (?, ?, ?)", (uid, title, year))
 				db.commit()
 
-		db.execute("update species set checked = 1 where name = ?", (species,))
-		db.commit()
-	
 	except Exception, e:
 		print "Unexpected error: " + str(e)
 		time.sleep(10)
